e233a78e6be6526fdcc18fb452f8eaf45ffc4f9f
cyberlord-007 <akashfzd2000@gmail.com>
2020-11-18 23:05:37 +0530

    started frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import SignIn from './components/SignIn';
 
-const App = () => {
-  return <div></div>;
-};
+const App = () => (
+  <BrowserRouter>
+    <Route path="/" exact component={SignIn} />
+  </BrowserRouter>
+);
 
 export default App;
